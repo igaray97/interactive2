8520819022038e19bfe11cba38e4d5a0675e30ea
Ileana <igaray@cca.edu>
2018-12-06 17:06:55 -0800

    slider

--- Exhibition/js/main_photo.js
+++ Exhibition/js/main_photo.js
@@ -6,20 +6,29 @@ $('button').click(function () {
 });
 
 
-let firstPhoto = document.querySelector('#photo')
-
-firstPhoto.addEventListener('click',function(){
-  document.body.classList.add('secondImage')
-  document.body.classList.remove('firstImage')
-  // document.body.classList.remove('secondImage')
-})
+// let firstPhoto = document.querySelector('#photo')
+//
+// firstPhoto.addEventListener('click',function(){
+//   document.body.classList.add('secondImage')
+//   document.body.classList.remove('firstImage')
+//   // document.body.classList.remove('secondImage')
+// })
 
 
 var range = document.getElementById("range");
 var imgOne = document.getElementsByClassName("img1")[0];
 var imgTwo = document.getElementsByClassName("img2")[0];
+var imgThree = document.getElementsByClassName("img3")[0];
+var imgFour = document.getElementsByClassName("img4")[0];
+var imgFive = document.getElementsByClassName("img5")[0];
+var imgSix = document.getElementsByClassName("img6")[0];
 
 range.addEventListener("change", function() {
     imgOne.style.opacity = this.value / this.max;
+    imgThree.style.opacity = this.value / this.max;
+    imgFour.style.opacity = this.value / this.max;
     imgTwo.style.opacity = (this.max - this.value) / this.max;
+    imgFive.style.opacity = (this.max - this.value) / this.max;
+    imgSix.style.opacity = (this.max - this.value) / this.max;
+
 });
